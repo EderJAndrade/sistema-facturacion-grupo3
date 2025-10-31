2157d4e7028786941197a2a84a8ccfb950bcf639
Karina Pita <109291624+chin1704@users.noreply.github.com>
2025-10-31 12:19:08 -0500

    Update FacturaSeeder.php
    encargado de poblar la tabla facturas con datos de prueba

--- database/seeders/FacturaSeeder.php
+++ database/seeders/FacturaSeeder.php
@@ -4,35 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Factura;
-use App\Models\DetalleFactura;
-use App\Models\Producto;
 
-class DetalleFacturaSeeder extends Seeder
+class FacturaSeeder extends Seeder
 {
     public function run(): void
     {
-        $facturas = Factura::all();
-
-        foreach ($facturas as $factura) {
-            // Seleccionar entre 1 y 5 productos distintos
-            $productos = Producto::inRandomOrder()->take(rand(1,5))->get();
-
-            foreach ($productos as $producto) {
-                $cantidad = rand(1, 5);
-                $precio = $producto->precio;
-
-                DetalleFactura::factory()->create([
-                    'factura_id' => $factura->id,
-                    'producto_id' => $producto->id,
-                    'cantidad' => $cantidad,
-                    'precio_unitario' => $precio,
-                    'subtotal' => round($cantidad * $precio, 2),
-                ]);
-            }
-
-            // Recalcular total de la factura
-            $factura->total = $factura->detalleFacturas()->sum('subtotal');
-            $factura->save();
-        }
+        // Solo se crean facturas, sin detalles
+        Factura::factory()->count(100)->create();
     }
 }
