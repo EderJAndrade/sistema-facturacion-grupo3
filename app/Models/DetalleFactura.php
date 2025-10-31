b6e8ef1dfd1b0e8e7f28690f2571126dd0a6fcc7
Karina Pita <109291624+chin1704@users.noreply.github.com>
2025-10-31 12:22:34 -0500

    Update DetalleFactura.php
    representa los detalles individuales, es decir líneas de productos o servicios, asociados a cada factura

--- app/Models/DetalleFactura.php
+++ app/Models/DetalleFactura.php
@@ -1,1 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleFactura extends Model
+{
+    use HasFactory;
+
+    protected $table = 'detalle_facturas';
+    protected $fillable = ['factura_id','producto_id','cantidad','precio_unitario','subtotal'];
+
+    public function factura()
+    {
+        return $this->belongsTo(Factura::class);
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+}
+
 
