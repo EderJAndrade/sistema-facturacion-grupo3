5ae8b2dd8c9dbebfcf98e11ed2f9871019b435dd
Karina Pita <109291624+chin1704@users.noreply.github.com>
2025-10-31 12:23:49 -0500

    Update Factura.php
    Cada registro de esta tabla corresponde a una factura emitida a un cliente y puede contener uno o varios detalles de los productos

--- app/Models/Factura.php
+++ app/Models/Factura.php
@@ -1,1 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Factura extends Model
+{
+    use HasFactory;
+
+    protected $table = 'facturas';
+    protected $fillable = ['cliente_id','fecha','total','estado'];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function detalleFacturas()
+    {
+        return $this->hasMany(DetalleFactura::class);
+    }
+}
+
 
